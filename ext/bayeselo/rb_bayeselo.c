8441d2abc5ee38e1758b3aed9e63d3aa72099e76
phillc <spyyderz@gmail.com>
2010-12-21 22:15:26 -0500

    looks like I am going the C extension route

---
+++ ext/bayeselo/rb_bayeselo.c
@@ -0,0 +1,6 @@
+#include <ruby.h>
+
+void Init_bayeselo()
+{
+  VALUE mBayeselo = rb_define_module("Bayeselo");
+}
---
+++ lib/bayeselo.rb
@@ -0,0 +1,4 @@
+Dir[File.dirname(__FILE__) + '/bayeselo/*.rb'].each {|file| require file }
+
+module Bayeselo
+end
